76f44e46ce75af8f21ec064def9cd85b5199acdb
ChungLab <chunglab@UWUSERs-iMac.local>
2025-09-30 12:47:50 -0500

    add requirements.txt

---
+++ ribo/lib/python3.11/site-packages/pysam/include/samtools/samtools_config_vars.h
@@ -0,0 +1,7 @@
+#define SAMTOOLS_CC "clang"
+#define SAMTOOLS_CPPFLAGS "-I/Users/runner/work/pysam/pysam/pysam -I/Users/runner/work/pysam/pysam/htslib -I/Users/runner/work/pysam/pysam/samtools -I/Users/runner/work/pysam/pysam/samtools/lz4 -I/Users/runner/work/pysam/pysam/bcftools -I/Users/runner/work/pysam/pysam -DBUILDING_WHEEL"
+#define SAMTOOLS_CFLAGS "-Wno-unused-result -Wsign-compare -Wunreachable-code -fno-common -dynamic -DNDEBUG -g -fwrapv -O3 -Wall   -g -arch x86_64  -Wno-unused -Wno-strict-prototypes -Wno-sign-compare -Wno-error=declaration-after-statement"
+#define SAMTOOLS_LDFLAGS "  -g -arch x86_64 -L/Users/runner/work/pysam/pysam/pysam -L/Users/runner/work/pysam/pysam -Lbuild/lib.macosx-10.9-x86_64-cpython-310/pysam -Lbuild/lib.macosx-10.9-x86_64-cpython-310/pysam -dynamiclib -rpath @loader_path -Wl,-headerpad_max_install_names -Wl,-install_name,@rpath/libcsamtools.cpython-310-darwin.so -Wl,-x"
+#define SAMTOOLS_LIBS "-lz -llzma -lbz2 -lz -lcurl -lchtslib.cpython-310-darwin"
+#define SAMTOOLS_HTSDIR "(unused)"
+#define SAMTOOLS_CURSES_LIB "(unused)"
---
+++ ribo/lib/python3.11/site-packages/ribopy/_version.py
@@ -0,0 +1,2 @@
+__version__ = '0.0.1'
+__format_version__ = '1.0'
